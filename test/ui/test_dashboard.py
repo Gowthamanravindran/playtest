4c2244823080053e092a5ab2613b1295487e9ff2
Gowthaman Ravindran <gowthaman.work@gmail.com>
2026-01-16 18:09:01 +0000

    Basic test setup

--- test/ui/test_dashboard.py
+++ test/ui/test_dashboard.py
@@ -2,9 +2,10 @@ import allure
 from pages.ui import DashboardPage
 from playwright.sync_api import expect
 
-@allure.epic("dashboard pages")
+@allure.suite("Dashboards")
 class TestDashboard:
     @allure.story("Dashboard Load")
+    @allure.title("Verify search element is visible")
     def test_dashboard(self, page, test_config):
         # Navigate to the base URL
         page.goto(test_config.data.ui.base_url)
